9b8e422569a0b16c8f1abe6aeb0fc45d07ebe000
Riya123-coder <83903808+Riya123-coder@users.noreply.github.com>
2022-04-21 20:29:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
-name_of_the_student_array = [];
+menu_list_array = ["Veg Margherita Pizza",
+                        "Chicken Tandoori Pizza",
+                        "Veg Supreme Pizza",
+                        "Paneer Tikka Pizza",
+                        "Deluxe Veggie Pizza",
+                        "Veg Extravaganza Pizza"];
     
-    function submit()
-    {
-        
-        var name_1 = document.getElementById("name_of_the_student_1").value;
-        var name_2 = document.getElementById("name_of_the_student_2").value;
-        var name_3 = document.getElementById("name_of_the_student_3").value;
-        var name_4 = document.getElementById("name_of_the_student_4").value;
-
-        name_of_the_student_array.push(name_1);
-        name_of_the_student_array.push(name_2);
-        name_of_the_student_array.push(name_3);
-        name_of_the_student_array.push(name_4);
-
-        console.log(name_of_the_student_array);
-        
-        document.getElementById("display_name").innerHTML = name_of_the_student_array;
-        document.getElementById("submit_button").style.display = "none";
-        document.getElementById("sort_button").style.display = "inline-block";
-
+    function getmenu(){
+        var htmldata="";
+        menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu 
     }
 
-function sorting()
-{
-    name_of_the_student_array.sort();
-    console.log(name_of_the_student_array);
-    document.getElementById("display_name").innerHTML = name_of_the_student_array;
+    function add_item(){
+        var htmldata;
+		var imgtags='<img id="im1" src="pizzaImg.png">'
+        var item=document.getElementById("add_item").value;
+        // use the sort function as - menu_list_array.sort();
+        htmldata="";
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+         document.getElementById("display_addedmenu").innerHTML = htmldata;
+		
+    }
+
+function add_top(){
+	var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
 }
